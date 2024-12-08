4f8755827f657535161950cd494b51b4cdc78cc3
pjr99 <pjr99@cornell.edu>
2024-12-08 13:59:12 -0500

    change db settings

--- weedfeed/settings.py
+++ weedfeed/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'weedfeed.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'weedfeed',
+        'USER': 'myadmin',
+        'PASSWORD': 'Conan_Stephens27',
+        'HOST': 'weedfeed.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
