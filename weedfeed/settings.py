e7695222b8ee36d8f537dbd54bb22b3c10f59d7e
Patrick <pjr99@cornell.edu>
2024-12-18 20:45:27 -0500

    refactoring

--- weedfeed/settings.py
+++ weedfeed/settings.py
@@ -20,10 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ljl#72^z$!iq^al5sh%1)c8#4xz-=l7i@%zvu#&e7h&fi2%7k-'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-7%l1!_y1!9x9o1z&+@5!g@2^zq8g2n@v3&7d1h6w@y2!$zr8a')
+
 DEBUG = True
 
 # settings.py
@@ -84,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'weedfeed',
         'USER': 'myadmin@weedfeed', 
-        'PASSWORD': 'Conan_Stephens27',
+        'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': 'weedfeed.postgres.database.azure.com',
         'PORT': '5432',
         'OPTIONS': {
