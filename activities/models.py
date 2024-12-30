402783ddaaad8b2db3e78f1f299d9dddf2665a50
Patrick <pjr99@cornell.edu>
2024-12-30 18:39:19 -0500

    change models

--- activities/models.py
+++ activities/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import migrations, models
 from users.models import CustomUser
 
 class Activity(models.Model):
@@ -7,7 +7,27 @@ class Activity(models.Model):
     strain_type = models.CharField(max_length=50, choices=[('indica', 'Indica'), ('sativa', 'Sativa'), ('hybrid', 'Hybrid')])
     rating = models.IntegerField()
     location = models.CharField(max_length=100, blank=True, null=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
+    latitude = models.FloatField(null=True, blank=True)  # Verify this exists
+    longitude = models.FloatField(null=True, blank=True) # Verify this exists
     notes = models.TextField(blank=True, null=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('activities', '0002_activity_user'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='activity',
+            name='latitude',
+            field=models.FloatField(blank=True, null=True),
+        ),
+        migrations.AddField(
+            model_name='activity',
+            name='longitude',
+            field=models.FloatField(blank=True, null=True),
+        ),
+    ]
